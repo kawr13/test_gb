dab9e750bc0196061ce6d187c4941daf3146e0e7
Alex <kawr13@gmail.com>
2023-03-22 22:59:08 +0500

    Add code

--- Test_code.py
+++ Test_code.py
@@ -1,1 +1,9 @@
-print ("hello world")
+numbers [1, 6, 8, 98, 4, 6, 7]
+max = numbers [0]
+index = 0
+size = 7
+while numbers [index] > max:
+    if max == numbers [index]:
+        size = size - 1       
+index = index + 1
+print ("max")
