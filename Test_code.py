d3b996222d9024ca99d2d366f87cd0d8e1475d07
kawr13 <62603910+kawr13@users.noreply.github.com>
2023-03-25 16:19:02 +0500

    Correct code

--- Test_code.py
+++ Test_code.py
@@ -1,10 +1,9 @@
 numbers = [1, 6, 8, 98, 4, 6, 7]
-max = numbers[index]
+max = numbers[0]
 index = 0
 size = 7
 while index < size:
     if numbers[index] > max:
-        max == numbers[index]
-    size = size - 1       
+        max = numbers[index]       
     index = index + 1
 print (max)
